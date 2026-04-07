c3e84233044e535d829d93053e9fa61b0dd83200
lytvynovamariia <litmarrom@gmail.com>
2026-04-07 15:38:20 +0200

    update

--- src/components/ml-ambulance-wl-editor/ml-ambulance-wl-editor.tsx
+++ src/components/ml-ambulance-wl-editor/ml-ambulance-wl-editor.tsx
@@ -189,66 +189,156 @@ export class MlAmbulanceWlEditor {
 
 
 render() {
+  if(this.errorMessage) {
+      return (
+      <Host>
+        <div class="error">{this.errorMessage}</div>
+      </Host>
+      )
+  }
   return (
-    <Host>
-      <md-filled-text-field label="Meno a Priezvisko" >
-        <md-icon slot="leading-icon">person</md-icon>
-      </md-filled-text-field>
+      <Host>
+        <form ref={el => this.formElement = el}>
+          <md-filled-text-field label="Meno a Priezvisko"
+            required pattern=".*\S.*" value={this.entry?.name}
+            oninput={ (ev: InputEvent) => {
+              if(this.entry) {this.entry.name = this.handleInputEvent(ev)}
+            } }>
+            <md-icon slot="leading-icon">person</md-icon>
+          </md-filled-text-field>
 
-      <md-filled-text-field label="Registračné číslo pacienta" >
-        <md-icon slot="leading-icon">fingerprint</md-icon>
-      </md-filled-text-field>
+          <md-filled-text-field label="Registračné číslo pacienta"
+            required pattern=".*\S.*" value={this.entry?.patientId}
+            oninput={ (ev: InputEvent) => {
+              if(this.entry) {this.entry.patientId = this.handleInputEvent(ev)}
+            } }>
+            <md-icon slot="leading-icon">fingerprint</md-icon>
+          </md-filled-text-field>
 
-      <md-filled-text-field label="Čakáte od" disabled>
-        <md-icon slot="leading-icon">watch_later</md-icon>
-      </md-filled-text-field>
+          <md-filled-text-field label="Čakáte od" disabled
+            value={this.entry?.waitingSince}>
+            <md-icon slot="leading-icon">watch_later</md-icon>
+          </md-filled-text-field>
 
-      <md-filled-select label="Dôvod návštevy">
-        <md-icon slot="leading-icon">sick</md-icon>
-        <md-select-option value="folowup">
-          <div slot="headline">Kontrola</div>
-        </md-select-option>
-        <md-select-option value="nausea">
-          <div slot="headline">Nevoľnosť</div>
-        </md-select-option>
-        <md-select-option value="fever">
-          <div slot="headline">Horúčka</div>
-        </md-select-option>
-        <md-select-option value="ache-in-throat">
-          <div slot="headline">Bolesti hrdla</div>
-        </md-select-option>
-      </md-filled-select>
+          <md-filled-select label="Dôvod návštevy"
+            value={this.entry?.condition?.code}
+            oninput = { (ev: InputEvent) => {
+              if(this.entry) {this.entry.condition.code = this.handleInputEvent(ev)}
+            } }>
+            <md-icon slot="leading-icon">sick</md-icon>
+            <md-select-option value="folowup">
+              <div slot="headline">Kontrola</div>
+            </md-select-option>
+            <md-select-option value="nausea">
+              <div slot="headline">Nevoľnosť</div>
+            </md-select-option>
+            <md-select-option value="fever">
+              <div slot="headline">Horúčka</div>
+            </md-select-option>
+            <md-select-option value="ache-in-throat">
+              <div slot="headline">Bolesti hrdla</div>
+            </md-select-option>
+          </md-filled-select>
+        </form>
 
-      <div class="duration-slider">
-        <span class="label">Predpokladaná doba trvania:&nbsp; </span>
-        <span class="label">{this.duration}</span>
-        <span class="label">&nbsp;minút</span>
-        <md-slider
-          min="2" max="45" value={this.duration} ticks labeled
-          oninput={this.handleSliderInput.bind(this)}></md-slider>
-      </div>
+        <div class="duration-slider">
+          <span class="label">Predpokladaná doba trvania:&nbsp; </span>
+          <span class="label">{this.duration}</span>
+          <span class="label">&nbsp;minút</span>
+          <md-slider
+            min="2" max="45" value={this.entry?.estimatedDurationMinutes || 15} ticks labeled
+            oninput={ (ev:InputEvent) => {
+              if(this.entry) {
+              this.entry.estimatedDurationMinutes
+                  = Number.parseInt(this.handleInputEvent(ev))};
+              this.handleSliderInput(ev)
+            } }></md-slider>
+        </div>
 
-      <md-divider></md-divider>
-      <div class="actions">
-        <md-filled-tonal-button id="delete"
-          onClick={() => this.editorClosed.emit("delete")}>
-          <md-icon slot="icon">delete</md-icon>
-          Zmazať
-        </md-filled-tonal-button>
-        <span class="stretch-fill"></span>
-        <md-outlined-button id="cancel"
-          onClick={() => this.editorClosed.emit("cancel")}>
-          Zrušiť
-        </md-outlined-button>
-        <md-filled-button id="confirm"
-          onClick={() => this.editorClosed.emit("store")}>
-          <md-icon slot="icon">save</md-icon>
-          Uložiť
-        </md-filled-button>
-      </div>
-    </Host>
-  );
+        <md-divider inset></md-divider>
+
+        <div class="actions">
+          <md-filled-tonal-button id="delete" disabled={ !this.entry }
+            onClick={() => this.deleteEntry()} >
+            <md-icon slot="icon">delete</md-icon>
+            Zmazať
+          </md-filled-tonal-button>
+          <span class="stretch-fill"></span>
+          <md-outlined-button id="cancel"
+            onClick={() => this.editorClosed.emit("cancel")}>
+            Zrušiť
+          </md-outlined-button>
+          <md-filled-button id="confirm"
+            onClick={() => this.updateEntry() }
+            >
+            <md-icon slot="icon">save</md-icon>
+            Uložiť
+          </md-filled-button>
+        </div>
+      </Host>
+    );
 }
+// render() {
+//   return (
+//     <Host>
+//       <md-filled-text-field label="Meno a Priezvisko" >
+//         <md-icon slot="leading-icon">person</md-icon>
+//       </md-filled-text-field>
+
+//       <md-filled-text-field label="Registračné číslo pacienta" >
+//         <md-icon slot="leading-icon">fingerprint</md-icon>
+//       </md-filled-text-field>
+
+//       <md-filled-text-field label="Čakáte od" disabled>
+//         <md-icon slot="leading-icon">watch_later</md-icon>
+//       </md-filled-text-field>
+
+//       <md-filled-select label="Dôvod návštevy">
+//         <md-icon slot="leading-icon">sick</md-icon>
+//         <md-select-option value="folowup">
+//           <div slot="headline">Kontrola</div>
+//         </md-select-option>
+//         <md-select-option value="nausea">
+//           <div slot="headline">Nevoľnosť</div>
+//         </md-select-option>
+//         <md-select-option value="fever">
+//           <div slot="headline">Horúčka</div>
+//         </md-select-option>
+//         <md-select-option value="ache-in-throat">
+//           <div slot="headline">Bolesti hrdla</div>
+//         </md-select-option>
+//       </md-filled-select>
+
+//       <div class="duration-slider">
+//         <span class="label">Predpokladaná doba trvania:&nbsp; </span>
+//         <span class="label">{this.duration}</span>
+//         <span class="label">&nbsp;minút</span>
+//         <md-slider
+//           min="2" max="45" value={this.duration} ticks labeled
+//           oninput={this.handleSliderInput.bind(this)}></md-slider>
+//       </div>
+
+//       <md-divider></md-divider>
+//       <div class="actions">
+//         <md-filled-tonal-button id="delete"
+//           onClick={() => this.editorClosed.emit("delete")}>
+//           <md-icon slot="icon">delete</md-icon>
+//           Zmazať
+//         </md-filled-tonal-button>
+//         <span class="stretch-fill"></span>
+//         <md-outlined-button id="cancel"
+//           onClick={() => this.editorClosed.emit("cancel")}>
+//           Zrušiť
+//         </md-outlined-button>
+//         <md-filled-button id="confirm"
+//           onClick={() => this.editorClosed.emit("store")}>
+//           <md-icon slot="icon">save</md-icon>
+//           Uložiť
+//         </md-filled-button>
+//       </div>
+//     </Host>
+//   );
+// }
 
 private renderConditions() {
   let conditions = this.conditions || [];
